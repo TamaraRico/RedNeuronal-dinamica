30d9dcc79aab84289b07d1a9ef4a3e14612c8208
Tamara Rico <tamara.rico@uabc.edu.mx>
2022-11-26 22:35:15 -0800

    Add files via upload

---
+++ normalizacion (2).py
@@ -0,0 +1,33 @@
+import numpy as np
+from sklearn import preprocessing
+
+def estandar(dat):
+    z_scaler = preprocessing.StandardScaler()
+    return z_scaler.fit_transform(dat)
+
+def minmax(dat):
+    mm_scaler = preprocessing.MinMaxScaler()
+    return mm_scaler.fit_transform(dat)
+
+def maxabs(dat):
+    ma_scaler = preprocessing.MaxAbsScaler
+    return ma_scaler.fit_transform(dat)
+
+def robust(dat):
+    r_scaler = preprocessing.RobustScaler()
+    return r_scaler.fit_transform(dat)
+
+class Normalizacion():
+    def __init__(self, normalizacion):
+        self.normalizacion = normalizacion
+
+    def Normalizacion(prediction, normalizacion):
+        if normalizacion == "estandar":
+            N = estandar(prediction)
+        elif normalizacion == "minmax":
+            N = minmax(prediction)
+        elif normalizacion == "robust":
+            N = maxabs(prediction)
+        elif normalizacion == "norma":
+            N = robust(prediction)
+        return N
